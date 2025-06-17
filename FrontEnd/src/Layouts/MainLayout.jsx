a7900aad18d6b3008dd11d0d526a63e6cf50d287
ikrambenallali <ikrambenallali2003@gmail.com>
2025-06-17 14:18:55 +0100

    merge

--- FrontEnd/src/Layouts/MainLayout.jsx
+++ FrontEnd/src/Layouts/MainLayout.jsx
@@ -2,12 +2,7 @@
 import React from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
-<<<<<<< HEAD
-import Service from '../Components/Service';
-import Donate from '../Components/Donate';
-=======
 import Service from '../Components/Home';
->>>>>>> bbede7515f320906b2e7d1842f28a0d06ceb2d0a
 
 export default function MainLayout({ children }) {
   return (
