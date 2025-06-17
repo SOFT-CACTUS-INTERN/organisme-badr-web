468711601a0d8266eec6e9bdda668cee6428819d
unknown <meriemelmecaniqgmail.com>
2025-06-17 12:33:23 +0200

    fixe some eroror

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
 
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: './', // <--- C’est important pour le déploiement Vercel
   plugins: [
     react(),
     tailwindcss(),
-  ],
+    
+  ],base:process.env.VITE_BASE_PATH||"organisme-badr-web/",
 })
