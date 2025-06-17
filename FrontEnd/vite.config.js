649ebb9ad04e75fa315097d04b69840f23ddbef8
unknown <meriemelmecaniqgmail.com>
2025-06-17 11:50:00 +0200

    Fix: clean install vite for Vercel

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
-
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: './', // <--- C’est important pour le déploiement Vercel
   plugins: [
     react(),
     tailwindcss(),
