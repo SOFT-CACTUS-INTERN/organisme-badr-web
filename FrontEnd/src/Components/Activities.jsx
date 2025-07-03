1b67cd2aa270dcad6413350fd0a68ef3b9f3b74c
unknown <meriemelmecaniqgmail.com>
2025-07-03 13:53:55 +0200

    component de activities

--- FrontEnd/src/Components/Activities.jsx
+++ FrontEnd/src/Components/Activities.jsx
@@ -1,18 +1,26 @@
+import React, { useState } from 'react'; // ← N’oublie pas d’importer useState !
 import '../assets/styles/activite.css';
 import activiteGraph7 from '../assets/svgs/graph7.svg';
 import activiteGraph14 from '../assets/svgs/graph14.svg';
 
-const ActiviteCard = ({ imageSrc, title, location, description,categorie }) => {
+const ActiviteCard = ({ imageSrc, title, location, description, categorie }) => {
   return (
     <div className="activite">
       <div className="image-div">
-       
-          <button className="btn-activite">
-            {categorie}
-            <div className="out"></div>
-          </button>
-    
-        <img className="activite-pecture" src={imageSrc} alt="Activité" />
+        <button className="btn-activite">
+          {categorie}
+          <div className="out"></div>
+        </button>
+        {/* <img className="activite-pecture" src={imageSrc} alt="Activité" /> */}
+
+      <div
+  className="activite-pecture"
+  style={{
+    backgroundImage: `url(${imageSrc})`
+   
+  }}
+></div>
+
       </div>
       <div className="activite-content">
         <h2>{title}</h2>
@@ -20,11 +28,11 @@ const ActiviteCard = ({ imageSrc, title, location, description,categorie }) => {
         <p>{description}</p>
         <div className="btn-groupe">
           <button className="left-btn">
-             <img className="activite-graph24" src={activiteGraph14} alt="Icon" />
+            <img className="activite-graph24" src={activiteGraph14} alt="Icon" />
             تواصل معنا
           </button>
           <button className="right-btn">
-           <img className="activite-graph24" src={activiteGraph14} alt="Icon" />
+          
             للتعرف على المزيد
           </button>
         </div>
@@ -34,7 +42,9 @@ const ActiviteCard = ({ imageSrc, title, location, description,categorie }) => {
 };
 
 const Activities = () => {
-  const activiteData = [
+  const [currentIndex, setCurrentIndex] = useState(0); 
+
+   const activiteData = [
     {
       imageSrc: "/organisme-badr-web/flags/study.png",
       title: "التعليم فتح لي فرص كنت أظنها مستحيلة، والآن أصبح لدي حلم أستطيع تحقيقه.",
@@ -42,27 +52,39 @@ const Activities = () => {
       description:
         "من ضمن الأنشطة التي تقوم بها منظمتنا غير الحكومية، تنظيم ورش عمل تعليمية في المجتمعات النائية بهدف تعزيز الوعي بأهمية التعليم في تحسين فرص الحياة. تهدف هذه الورش إلى تقديم معلومات قيمة للأطفال والشباب حول كيف يمكن للتعليم أن يساعدهم في تحقيق طموحاتهم وتطوير مهاراتهم.",
    
-      categorie:"الطبخ"
+      categorie:"التعليم"
     },
     {
-      imageSrc: "/organisme-badr-web/flags/study.png",
+      imageSrc: "/organisme-badr-web/flags/boy.png",
       title: "التعليم فتح لي فرص كنت أظنها مستحيلة، والآن أصبح لدي حلم أستطيع تحقيقه.",
       location: "Morocco, Berkane",
       description:
         "من ضمن الأنشطة التي تقوم بها منظمتنا غير الحكومية، تنظيم ورش عمل تعليمية في المجتمعات النائية بهدف تعزيز الوعي بأهمية التعليم في تحسين فرص الحياة. تهدف هذه الورش إلى تقديم معلومات قيمة للأطفال والشباب حول كيف يمكن للتعليم أن يساعدهم في تحقيق طموحاتهم وتطوير مهاراتهم.",
       
-      categorie:"التعليم"
+      categorie:"الطبخ"
     },
     {
-      imageSrc: "/organisme-badr-web/flags/study.png",
+      imageSrc: "/organisme-badr-web/flags/girls.png",
       title: "التعليم فتح لي فرص كنت أظنها مستحيلة، والآن أصبح لدي حلم أستطيع تحقيقه.",
       location: "Morocco, Berkane",
       description:
         "من ضمن الأنشطة التي تقوم بها منظمتنا غير الحكومية، تنظيم ورش عمل تعليمية في المجتمعات النائية بهدف تعزيز الوعي بأهمية التعليم في تحسين فرص الحياة. تهدف هذه الورش إلى تقديم معلومات قيمة للأطفال والشباب حول كيف يمكن للتعليم أن يساعدهم في تحقيق طموحاتهم وتطوير مهاراتهم.",
      
-      categorie:"الطبخ"
+      categorie:"mosika "
     },
   ];
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === activiteData.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? activiteData.length - 1 : prevIndex - 1
+    );
+  };
 
   return (
     <section className="activities-section">
@@ -78,15 +100,16 @@ const Activities = () => {
 
       <div className="activite-p">
         <p>
-          هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث
-          يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى
+       هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى 
         </p>
       </div>
 
       <div className="activite-groupe">
-        {/* {activiteData.map((item, index) => (
-          <ActiviteCard key={index} {...item} />
-        ))} */}
+        <button className='prev' onClick={handlePrev}> <div className='lgn'>❮</div> </button>
+
+        <ActiviteCard {...activiteData[currentIndex]} />
+
+        <button className='next' onClick={handleNext}> <div className='lgn'>❯</div> </button>
       </div>
     </section>
   );
