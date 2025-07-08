1c8a36fe3dd017fee2232c92762987f6b9ea10b9
ikrambenallali <ikrambenallali2003@gmail.com>
2025-07-08 10:52:37 +0100

    organisme disktop

--- FrontEnd/src/Components/Organisme.jsx
+++ FrontEnd/src/Components/Organisme.jsx
@@ -13,14 +13,14 @@ function Organisme() {
     const navigate = useNavigate();
 
     return (
-        <section className=" w-full  min-h-screen flex justify-center items-center " style={{ padding: '2rem ' }}>
+        <section className=" w-full  min-h-screen flex justify-center items-center " >
 
             <img
                 src={chms}
                 alt="chms"
-                className="  absolute top-[9%] lg:top-86 md:top-[10%] md:right-[62%]  right-32 lg:right-[58%]"
+                className="  absolute top-[9%] lg:top-[6%] md:top-[10%] md:right-[62%]  right-32 lg:right-[58%]"
             />
-            <div className="bg-white rounded-2xl   shadow-md lg:w-3/4 md:w-full h-screen sm:w-3/4 w-full sm:h-80 flex flex-col md:flex-row justify-center md">
+            <div className="bg-white rounded-2xl   shadow-md lg:w-3/4 md:w-full h-screen sm:w-3/4 w-full sm:h-100 flex flex-col md:flex-row justify-center md">
                 <img
                     src={or}
                     alt="or"
@@ -30,21 +30,21 @@ function Organisme() {
                 <img
                     src={focht}
                     alt="focht"
-                    className=" absolute  "
+                    className=" absolute  lg:top-[11%]"
                 />
                 <img
                     src={focht2}
                     alt="focht2"
-                    className=" absolute right-[45%] sm:block lg:block md:hidden hidden 2xl:block  "
+                    className=" absolute right-[45%] sm:block lg:block md:hidden hidden 2xl:block   lg:top-[11%]"
                 />
-                <div className="container relative w-full md:w-1/2 flex items-end justify-center mb-6 md:mb-0 z-10 min-h-[250px] sm:min-h-0">
+                <div className="container relative w-full md:w-1/2 flex items-end justify-center mb-6 md:mb-0 z-10 min-h-[250px] sm:min-h-0 lg:w-1/2">
                     <div className="relative bg-[#FFB700] rounded-t-full flex h-40 sm:h-48 md:h-56 w-11/12 mx-auto "></div>
                     <img src="/organisme-badr-web/images/photo4.png" alt="Vite logo"
                         className="image1  absolute w-10/12 sm:w-9/12 md:w-full max-w-xs md:max-w-none  -translate-x-[2%] bottom-0"
                     />
 
                 </div>
-                <div className='w-full md:w-1/2 px-4 md:px-0'>
+                <div className='w-full md:w-1/2 px-4 md:px-0 lg:w-1/2'>
                     <div className="flex justify-center items-center gap-2 md:gap-4 lg:justify-end" style={{ padding: '1rem' }}>
                         <h1 className="text-[#38B000] text-lg md:text-xl text-center lg:text-right">
                             منظمة <span className="text-[#FFB700]">البدر</span>
@@ -57,13 +57,13 @@ function Organisme() {
                     </div>
 
                     <div>
-                        <h2 className='font-bold text-xl sm:text-2xl md:text-3xl lg:text-3xl text-center md:text-right' style={{ padding: '0 1rem' }}>
+                        <h2 className='font-bold text-xl sm:text-2xl md:text-3xl lg:text-3xl text-center md:text-right' style={{ padding: '1.5rem' }}>
                             منظمة البدر للأشخاص ذوي الإعاقة
                             و المرأة و الطفل
                         </h2>
                         <div className='flex flex-col md:flex-row'>
 
-                            <p className='text-right text-sm md:text-base' style={{ padding: '0 1rem' }}>منظمة البدر للأشخاص ذوي الإعاقة والمرأة والطفل هي منظمة تهدف إلى تقديم الدعم والرعاية للأفراد الذين يعانون من الإعاقة، بالإضافة إلى الاهتمام بقضايا المرأة والطفل</p>
+                            <p className='text-right text-sm md:text-base text-[#797979]' style={{ padding: '0 1rem' }}>منظمة البدر للأشخاص ذوي الإعاقة والمرأة والطفل هي منظمة تهدف إلى تقديم الدعم والرعاية للأفراد الذين يعانون من الإعاقة، بالإضافة إلى الاهتمام بقضايا المرأة والطفل</p>
                             <img
                                 src={ligne1}
                                 alt="ligne1"
@@ -76,12 +76,12 @@ function Organisme() {
                     <div className="flex flex-col sm:flex-row justify-end gap-2" style={{ padding: '1.5rem' }}>
                         <button
                             onClick={() => navigate('/Contact')}
-                            className="button2 !bg-[#38B000] text-white font-bold text-sm md:text-base px-3 py-2 md:px-4 md:py-2 flex justify-center"
-                            style={{ padding: '0 1rem' }}
+                            className="button2 !bg-[#38B000] text-white   text-sm md:text-base px-3 py-2 md:px-4 md:py-2 flex justify-center"
+                            style={{ padding: '0 1rem',fontFamily: 'normal' }}
                         >
                             تواصل معنا
                         </button>
-                        <button className="button1 !bg-[#FFD000] text-white font-bold text-sm md:text-base px-3 py-2 md:px-4 md:py-2 flex justify-center" style={{ padding: '0 1rem' }}>
+                        <button className="button1 !bg-[#FFD000] text-white   text-sm md:text-base px-3 py-2 md:px-4 md:py-2 flex justify-center" style={{ padding: '0 1rem' ,fontFamily: 'normal' }}>
                             منظمة غير حكومية
                         </button>
                     </div>
