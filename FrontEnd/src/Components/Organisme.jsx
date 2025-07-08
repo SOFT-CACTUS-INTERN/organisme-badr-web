ebeef89b77812eefa443d28adaf9bd3153cb7427
ikrambenallali <ikrambenallali2003@gmail.com>
2025-07-08 14:54:15 +0100

    who components

--- FrontEnd/src/Components/Organisme.jsx
+++ FrontEnd/src/Components/Organisme.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import fleche3 from '../assets/svg/fleche3.svg';
 import ligne1 from '../assets/svg/ligne1.svg';
-import chms from '../assets/svg/chms.svg';
-import or from '../assets/svg/or.svg';
-import focht from '../assets/svg/focht.svg';
-import focht2 from '../assets/svg/focht2.svg';
+// import chms from '../assets/svg/chms.svg';
+// import or from '../assets/svg/or.svg';
+// import focht from '../assets/svg/focht.svg';
+// import focht2 from '../assets/svg/focht2.svg';
+import photo1 from '../assets/svg/photo1.svg';
 import '../assets/styles/organisme.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -15,49 +16,51 @@ function Organisme() {
     return (
         <section className=" w-full  min-h-screen flex justify-center items-center " >
 
-            <img
+            {/* <img
                 src={chms}
                 alt="chms"
-                className="  absolute top-[9%] lg:top-[6%] md:top-[10%] md:right-[62%]  right-32 lg:right-[58%]"
-            />
-            <div className="bg-white rounded-2xl   shadow-md lg:w-3/4 md:w-full h-screen sm:w-3/4 w-full sm:h-100 flex flex-col md:flex-row justify-center md">
-                <img
+                className="icon1  absolute top-[9%] lg:top-[6%] md:top-[10%] md:right-[62%]  right-32 lg:right-[58%]"
+            /> */}
+            <div className="organisme-container  bg-white rounded-2xl   shadow-md lg:w-3/4 md:w-full h-screen sm:w-3/4 w-full sm:h-100 flex flex-col md:flex-row justify-center md">
+                {/* <img
                     src={or}
                     alt="or"
-                    className="absolute lg:right-[75%] md:right-[80%] right-[65%] top-[8%] lg:top-[10%] md:top-[10%]"
+                    className="coeur1 absolute lg:right-[75%] md:right-[80%] right-[65%] top-[8%] lg:top-[10%] md:top-[10%]"
                 />
 
                 <img
                     src={focht}
                     alt="focht"
-                    className=" absolute  lg:top-[11%]"
+                    className="icon2 absolute  lg:top-[11%]"
                 />
                 <img
                     src={focht2}
                     alt="focht2"
-                    className=" absolute right-[45%] sm:block lg:block md:hidden hidden 2xl:block   lg:top-[11%]"
-                />
-                <div className="container relative w-full md:w-1/2 flex items-end justify-center mb-6 md:mb-0 z-10 min-h-[250px] sm:min-h-0 lg:w-1/2">
-                    <div className="relative bg-[#FFB700] rounded-t-full flex h-40 sm:h-48 md:h-56 w-11/12 mx-auto "></div>
-                    <img src="/organisme-badr-web/images/photo4.png" alt="Vite logo"
-                        className="image1  absolute w-10/12 sm:w-9/12 md:w-full max-w-xs md:max-w-none  -translate-x-[2%] bottom-0"
+                    className="icon3 absolute right-[45%]   lg:top-[11%]"
+                /> */}
+                <div className="container relative w-full md:w-1/2 flex items-end justify-center md:mb-0 z-10 min-h-[250px] sm:min-h-0 lg:w-1/2">
+                    {/* <div className="relative bg-[#FFB700] rounded-t-full flex h-40 sm:h-48 md:h-56 w-11/12 mx-auto "></div> */}
+                    <img src={photo1} alt="Vite logo"
+                        className="image1  absolute w-10/12 sm:w-9/12 md:w-full max-w-xs md:max-w-none   bottom-0"     style={{ clipPath: 'inset(1mm 0 0 0)' }}
+
+
                     />
 
                 </div>
                 <div className='w-full md:w-1/2 px-4 md:px-0 lg:w-1/2'>
-                    <div className="flex justify-center items-center gap-2 md:gap-4 lg:justify-end" style={{ padding: '1rem' }}>
+                    <div className="flex justify-end items-center gap-2 md:gap-4 lg:justify-end " style={{ padding: '1rem' }}>
                         <h1 className="text-[#38B000] text-lg md:text-xl text-center lg:text-right">
                             منظمة <span className="text-[#FFB700]">البدر</span>
                         </h1>
                         <img
                             src={fleche3}
                             alt="fleche3"
-                            className="sm:block lg:block md:hidden hidden 2xl:block w-6 h-6 md:w-8 md:h-8"
+                            className="sm:block lg:block md:hidden w-1/6 2xl:block  h-6 md:w-8 md:h-8"
                         />
                     </div>
 
                     <div>
-                        <h2 className='font-bold text-xl sm:text-2xl md:text-3xl lg:text-3xl text-center md:text-right' style={{ padding: '1.5rem' }}>
+                        <h2 className='font-bold text-xl   sm:text-2xl md:text-3xl lg:text-3xl text-right md:text-right' style={{ padding: '1.5rem' }}>
                             منظمة البدر للأشخاص ذوي الإعاقة
                             و المرأة و الطفل
                         </h2>
@@ -73,7 +76,7 @@ function Organisme() {
                         </div>
 
                     </div>
-                    <div className="flex flex-col sm:flex-row justify-end gap-2" style={{ padding: '1.5rem' }}>
+                    <div className="flex flex-row sm:flex-row justify-end gap-2" style={{ padding: '1.5rem' }}>
                         <button
                             onClick={() => navigate('/Contact')}
                             className="button2 !bg-[#38B000] text-white   text-sm md:text-base px-3 py-2 md:px-4 md:py-2 flex justify-center"
