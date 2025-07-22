5b9188548f0be60a5a8b025737d51761cd92a8fc
ikrambenallali <ikrambenallali2003@gmail.com>
2025-07-22 11:20:48 +0100

    lat commit before merge with organisme

--- FrontEnd/src/Components/Contact.jsx
+++ FrontEnd/src/Components/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
         <>
 <div className=' min-h-screen flex flex-col ' >
 
-            {/* <Formule /> */}
+            <Formule />
 
 
-            {/* <Donate /> */}
+            <Donate />
 
 
             <Local />
