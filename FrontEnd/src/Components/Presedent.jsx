b2d87d99c098838f882ba7431b1a8ba1a0a1598b
ikrambenallali <ikrambenallali2003@gmail.com>
2025-07-24 15:24:53 +0100

    t

--- FrontEnd/src/Components/Presedent.jsx
+++ FrontEnd/src/Components/Presedent.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../assets/styles/presedent.css';
+import '../assets/styles/who.css';
 import green from '../assets/svg/green.svg';
 import fleche1 from '../assets/svg/fleche1.svg';
 import fleche2 from '../assets/svg/fleche2.svg';
@@ -7,6 +8,8 @@ import ligne from '../assets/svg/ligne.svg';
 import col from '../assets/svg/col.svg';
 import presedant from '../assets/svg/presedant.svg';
 import Title from './Title'
+import social from '../assets/svg/social.svg';
+
 
 
 function Presedent() {
@@ -111,6 +114,31 @@ function Presedent() {
         </div>
       </div>
 
+ <div className='partenaire '>
+                    <div className='flex '>
+
+                        <div className="flex items-center justify-center w-full gap-4">
+                            <div className="ligne1 border-[#E5E5E5] border-t-2 flex-grow" style={{ width: '70%' }}></div>
+
+                            <h1 className="isam text-[#38B000] whitespace-nowrap text-center flex justify-end " >
+                                شركائنا
+                            </h1>
+
+                            <div className="ligne2 border-[#E5E5E5] border-t-2 flex-grow"></div>
+                        </div>
+
+                    </div>
+                    <div className="slider-container    " >
+                        <div className="slider-track " >
+                            <img src={social} alt="social" />
+                            <img src={social} alt="social" />
+                            <img src={social} alt="social" />
+                        </div>
+                    </div>
+
+                    <hr className="ligne3 border-[#E5E5E5] flex-grow border-t-2 " style={{ width: '100%' }} />
+
+                </div>
     </section>
   )
 }
