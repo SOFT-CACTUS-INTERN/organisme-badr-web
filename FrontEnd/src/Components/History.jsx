4f9f84927014f8fc1b11df6a89900df1c06c504b
unknown <meriemelmecaniqgmail.com>
2025-07-30 18:11:53 +0200

    componant de timelineItem

--- FrontEnd/src/Components/History.jsx
+++ FrontEnd/src/Components/History.jsx
@@ -1,33 +1,35 @@
-
- import  graph45 from '../assets/svgs/graph45.svg'
- import '../assets/styles/history.css';
- import HistoryTitle from './HistoryTitle';
- import TimelineItem from './TimelineItem';
-
+import graph45 from '../assets/svgs/graph45.svg';
+import '../assets/styles/history.css';
+import HistoryTitle from './HistoryTitle';
+import TimelineItem from './TimelineItem';
 import timelineData from '../data.json';
-console.log(timelineData);
-
 
 const History = () => {
-    return (  
-       <section className="history-section">
+  return (
+    <section className="history-section">
+      <HistoryTitle />
 
-<HistoryTitle />
-  <div className="histories">
-        {timelineData.map((item, i) => (
-          <TimelineItem key={i} {...item} />
-        ))}
+     
+
+      <div className="histories">
+        <div className="histories-right">
+          {timelineData
+            .filter(item => item.side === 'right')
+            .map((item, i) => (
+              <TimelineItem key={i} {...item} />
+            ))}
+        </div>
+        <div className='histories-ligne'>testddddd</div>
+        <div className="histories-left">
+          {timelineData
+            .filter(item => item.side === 'left')
+            .map((item, i) => (
+              <TimelineItem key={i} {...item} />
+            ))}
+        </div>
       </div>
+    </section>
+  );
+};
 
-
-
-
-
-
-
-
-       </section>
-    );
-}
- 
 export default History;
--- FrontEnd/src/Components/TimelineItem.jsx
+++ FrontEnd/src/Components/TimelineItem.jsx
@@ -1,7 +1,23 @@
-const TimelineItem = () => {
-    return (
-        <></>
-      );
-}
- 
+import '../assets/styles/history.css';
+import graph47 from '../assets/svgs/graph47.svg';
+
+const TimelineItem = ({ year, description, side, color }) => {
+  return (
+    <div className='timeline-item' >
+      <div className="timeline-year" style={{ backgroundColor: color }}>{year}</div>
+      <ul className="timeline-description">
+   {description.map((line, i) => (
+  <li className={`line ${side === 'right' ? 'reverse' : ''}`} key={i}>
+    <img src={graph47} alt="" />
+    <span>{line}</span>
+  </li>
+))}
+
+
+      </ul>
+      <img src={graph47} alt="Décor SVG" className="timeline-svg" />
+    </div>
+  );
+};
+
 export default TimelineItem;
