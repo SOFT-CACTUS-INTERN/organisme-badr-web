27b7d2c7a378ea151138bbc8d1de9efc64e705ad
unknown <meriemelmecaniqgmail.com>
2025-07-30 14:08:27 +0200

    start componanat History

---
+++ FrontEnd/src/Components/HistoryTitle.jsx
@@ -0,0 +1,15 @@
+import graph45 from '../assets/svgs/graph45.svg';
+
+const HistoryTitle = () => (
+  <div className="history-title">
+    <img className="graph45 left" src={graph45} alt="" />
+    <h1>
+      <span className="quote">"</span>
+      مسيرة البدر: محطات مضيئة منذ التأسيس
+      <span className="quote">"</span>
+    </h1>
+    <img className="graph45 right" src={graph45} alt="" />
+  </div>
+);
+
+export default HistoryTitle;
