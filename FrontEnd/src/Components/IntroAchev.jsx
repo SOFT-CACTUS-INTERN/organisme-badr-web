91993cc0affce0b57f51bca97c3b854fac373510
ikrambenallali <ikrambenallali2003@gmail.com>
2025-07-31 11:45:08 +0100

    introAchev

--- FrontEnd/src/Components/IntroAchev.jsx
+++ FrontEnd/src/Components/IntroAchev.jsx
@@ -1,8 +1,17 @@
-const IntroAchev = () => {
-    return ( 
-        <></>
-     );
-   
+import React from 'react'
+import Title from './Title'
+
+function IntroAchev() {
+  return (
+  <section>
+        <Title title="أنشطة الجمعية منذ التأسيس" mini_title="مسيرة حافلة بالعطاء منذ التأسيس" />
+        <div className=''>
+            <img src="/organisme-badr-web/images/photo11.png" alt="" className='2xl:w-full' />
+        </div>
+
+
+  </section>
+  )
 }
- 
-export default IntroAchev;
+
+export default IntroAchev
